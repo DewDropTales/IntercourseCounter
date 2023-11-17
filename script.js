83834ca993ffa9bbc402881a08ff8f730e57d3e0
DewDrop <100445832+DewDropTales@users.noreply.github.com>
2023-11-17 15:36:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch(worldBankApiEndpoint)
     .then(response => response.json())
     .then(data => {
-      // Extract the latest population data from the API response
       const latestData = data[1][0];
       const currentPopulation = latestData.value;
 
-      // Use the population data to calculate the estimated people per second
+      // Calculate the estimated people per second
       const weeklyIntercourse = (1 / 3) * currentPopulation;
       const perSecond = weeklyIntercourse / (7 * 24 * 60 * 60);
 
       // Display the result on the webpage
-      const resultElement = document.getElementById('output');
-      resultElement.textContent = perSecond.toFixed(2);
+      const intercourseResultElement = document.getElementById('output');
+      intercourseResultElement.textContent = perSecond.toFixed(2);
+
+      // Calculate the estimated pregnancies per second
+      const pregnancyRate = 0.1; // Adjust this rate as needed
+      const pregnanciesPerSecond = perSecond * pregnancyRate;
+
+      // Display the pregnancy result on the webpage
+      const pregnancyResultElement = document.getElementById('pregnancyOutput');
+      pregnancyResultElement.textContent = pregnanciesPerSecond.toFixed(2);
     })
     .catch(error => {
       console.error('Error fetching data from the World Bank API:', error);
       // Handle errors, e.g., display a message to the user
     });
 });
+
+function openTab(tabId) {
+  const tabs = document.getElementsByClassName('tab');
+  for (const tab of tabs) {
+    tab.style.display = 'none';
+  }
+  document.getElementById(tabId).style.display = 'block';
+}
