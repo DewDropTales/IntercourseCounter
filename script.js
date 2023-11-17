3fa3c281fb96e4954c71dcfc8ed550b5f6006814
DewDrop <100445832+DewDropTales@users.noreply.github.com>
2023-11-17 16:03:22 +0100

    Update script.js
    1. **Worldometer Endpoint:**
       - Introduced a new constant named `worldometersEndpoint` that holds the URL of the Worldometer page containing world population information.
    
    2. **Fetching HTML Content:**
       - Used the `fetch` API to make an HTTP request to the Worldometer page and retrieve its HTML content.
    
    3. **Parsing HTML with Temporary Element:**
       - Created a temporary `div` element (`tempElement`) to parse the fetched HTML content. This is done by setting the HTML content of the div to the fetched HTML.
    
    4. **Finding Population Element:**
       - Utilized the `querySelector` method to locate the specific element on the Worldometer page that contains information about the world population. This assumes the structure of the page remains the same.
    
    5. **Extracting Population Text:**
       - Extracted the text content of the identified population element and trimmed any leading or trailing whitespaces.
    
    6. **Extracting Numeric Population:**
       - Used a regular expression (`replace(/[^0-9]/g, '')`) to extract only the numeric part from the population text. This assumes that the population is formatted as "Current World Population: 7,925,208,690".
    
    7. **Calculating and Displaying Results:**
       - Calculated the estimated people per second based on the extracted world population and continued with the calculation logic as before.
    
    8. **Error Handling:**
       - Implemented error handling using the `catch` block to log any errors that may occur during the fetch operation. Additionally, you can include user-friendly error handling to provide feedback to the user in case of issues.

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const worldBankApiEndpoint = 'https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json';
+  const worldometersEndpoint = 'https://www.worldometers.info/world-population/';
 
-  // Fetch population data from the World Bank API
-  fetch(worldBankApiEndpoint)
-    .then(response => response.json())
-    .then(data => {
-      const latestData = data[1][0];
-      const currentPopulation = latestData.value;
+  // Fetch the HTML content of the Worldometer page
+  fetch(worldometersEndpoint)
+    .then(response => response.text())
+    .then(html => {
+      // Create a temporary DOM element to parse the HTML
+      const tempElement = document.createElement('div');
+      tempElement.innerHTML = html;
 
-      // Calculate the estimated people per second
+      // Find the element containing the world population
+      const populationElement = tempElement.querySelector('.col-md-8.country-pop-description');
+
+      // Extract the world population text
+      const populationText = populationElement.textContent.trim();
+
+      // Extract the numeric part from the text (assuming it's formatted like "Current World Population: 7,925,208,690")
+      const currentPopulation = parseInt(populationText.replace(/[^0-9]/g, ''), 10);
+
+      // Calculate and display the estimated people per second
       const weeklyIntercourse = (1 / 3) * currentPopulation;
       const perSecond = weeklyIntercourse / (7 * 24 * 60 * 60);
 
       // Display the result on the webpage
       const intercourseResultElement = document.getElementById('output');
       intercourseResultElement.textContent = perSecond.toFixed(2);
-
-      // Calculate the estimated pregnancies per second
-      const pregnancyRate = 0.1; // Adjust this rate as needed
-      const pregnanciesPerSecond = perSecond * pregnancyRate;
-
-      // Display the pregnancy result on the webpage
-      const pregnancyResultElement = document.getElementById('pregnancyOutput');
-      pregnancyResultElement.textContent = pregnanciesPerSecond.toFixed(2);
     })
     .catch(error => {
-      console.error('Error fetching data from the World Bank API:', error);
+      console.error('Error fetching data from Worldometer:', error);
       // Handle errors, e.g., display a message to the user
     });
 });
-
-function openTab(tabId) {
-  const tabs = document.getElementsByClassName('tab');
-  for (const tab of tabs) {
-    tab.style.display = 'none';
-  }
-  document.getElementById(tabId).style.display = 'block';
-}
